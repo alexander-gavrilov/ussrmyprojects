70fd3e9e3f3014b2bc10de5553b364eec0194197
alexander.v.gavrilov <alexander.v.gavrilov@fe8489ff-6958-126e-9046-dc8df76c2bf4>
2011-03-16 07:17:19 +0000

    перед изменением формы диалога

---
+++ trunk/Poll/Components/AddChangeRecordForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Components
+{
+    public partial class AddChangeRecordForm : Form
+    {
+        public AddChangeRecordForm()
+        {
+            InitializeComponent();
+        }
+
+        private void acceptChangesButton_Click(object sender, EventArgs e)
+        {
+            acceptChanges();
+        }
+        public virtual void acceptChanges()
+        {
+            
+        }
+
+        private void rejectChangesButton_Click(object sender, EventArgs e)
+        {
+            rejectChanges();
+        }
+        public virtual void rejectChanges()
+        {
+            
+        }
+    }
+}
